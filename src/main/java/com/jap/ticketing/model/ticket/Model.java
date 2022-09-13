307cdc20481e6b4c0587691e184dbdee57351e0a
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 10:24:17 +0530

    added the private attributes in model class according to csv file

--- src/main/java/com/jap/ticketing/model/ticket/Model.java
+++ src/main/java/com/jap/ticketing/model/ticket/Model.java
@@ -8,4 +8,16 @@
 package com.jap.ticketing.model.ticket;
 
 public class Model {
+
+    private String scheduleNumber;
+    private String routeNumber;
+    private int ticketStopFromId;
+    private int ticketFromStopSequenceNumber;
+    private int ticketTillStopId;
+    private int ticketTillStopSeqNo;
+    private String ticketDate;
+    private String ticketTime;
+    private int totalTicketAmount;
+    private double travelledKm;
+
 }
