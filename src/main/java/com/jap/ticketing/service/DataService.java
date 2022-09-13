08ace38c4211ae941dd419ed85092c2472dd5cb1
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 10:15:56 +0530

    created a new file DataService.java in com.jap.ticketing.service package

---
+++ src/main/java/com/jap/ticketing/service/DataService.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name : Seetha Lakshmi.G.M
+ *Date : 13-09-2022
+ *Created With : IntelliJ IDEA Community Edition
+ */
+
+
+package com.jap.ticketing.service;
+
+public class DataService {
+}
