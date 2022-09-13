65ded9725aa15e755b5d4dcf068a3954cdd4c5fa
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 12:48:29 +0530

    created test cases for FileReaderService class

--- src/test/java/com/jap/ticketing/service/FileReaderServiceTest.java
+++ src/test/java/com/jap/ticketing/service/FileReaderServiceTest.java
@@ -1,20 +1,37 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.ticket.Model;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.util.List;
+
 class FileReaderServiceTest {
+    FileReaderService fileReaderService = null;
+    String fileName = "src/test/resources/sample.csv";
 
     @BeforeEach
     void setUp() {
+        fileReaderService = new FileReaderService();
     }
 
     @AfterEach
     void tearDown() {
+        fileReaderService = null;
     }
 
     @Test
-    void readFileLineByLine() {
+    public void readFileLineByLineSuccess() throws ParseException {
+        List<Model> expectedOutput = fileReaderService.readFileLineByLine(fileName);
+        Assertions.assertEquals(49, expectedOutput.size());
+    }
+
+    @Test
+    public void readFileLineByLineFailure() throws NumberFormatException {
+        List<Model> expectedOutput = fileReaderService.readFileLineByLine(fileName);
+        Assertions.assertNotEquals(50, expectedOutput.size());
     }
 }
