4e5ab059d109be9ba626116a1ce6bff11feabd82
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 10:26:30 +0530

    added getter and setter methods in model class

--- src/main/java/com/jap/ticketing/model/ticket/Model.java
+++ src/main/java/com/jap/ticketing/model/ticket/Model.java
@@ -23,16 +23,7 @@ public class Model {
     public Model() {
     }
 
-    public Model(String scheduleNumber,
-                 String routeNumber,
-                 int ticketStopFromId,
-                 int ticketFromStopSequenceNumber,
-                 int ticketTillStopId,
-                 int ticketTillStopSeqNo,
-                 String ticketDate,
-                 String ticketTime,
-                 int totalTicketAmount,
-                 double travelledKm) {
+    public Model(String scheduleNumber, String routeNumber, int ticketStopFromId, int ticketFromStopSequenceNumber, int ticketTillStopId, int ticketTillStopSeqNo, String ticketDate, String ticketTime, int totalTicketAmount, double travelledKm) {
         this.scheduleNumber = scheduleNumber;
         this.routeNumber = routeNumber;
         this.ticketStopFromId = ticketStopFromId;
@@ -42,6 +33,86 @@ public class Model {
         this.ticketDate = ticketDate;
         this.ticketTime = ticketTime;
         this.totalTicketAmount = totalTicketAmount;
+        this.travelledKm = travelledKm;
+    }
+
+    public String getScheduleNumber() {
+        return scheduleNumber;
+    }
+
+    public void setScheduleNumber(String scheduleNumber) {
+        this.scheduleNumber = scheduleNumber;
+    }
+
+    public String getRouteNumber() {
+        return routeNumber;
+    }
+
+    public void setRouteNumber(String routeNumber) {
+        this.routeNumber = routeNumber;
+    }
+
+    public int getTicketStopFromId() {
+        return ticketStopFromId;
+    }
+
+    public void setTicketStopFromId(int ticketStopFromId) {
+        this.ticketStopFromId = ticketStopFromId;
+    }
+
+    public int getTicketFromStopSequenceNumber() {
+        return ticketFromStopSequenceNumber;
+    }
+
+    public void setTicketFromStopSequenceNumber(int ticketFromStopSequenceNumber) {
+        this.ticketFromStopSequenceNumber = ticketFromStopSequenceNumber;
+    }
+
+    public int getTicketTillStopId() {
+        return ticketTillStopId;
+    }
+
+    public void setTicketTillStopId(int ticketTillStopId) {
+        this.ticketTillStopId = ticketTillStopId;
+    }
+
+    public int getTicketTillStopSeqNo() {
+        return ticketTillStopSeqNo;
+    }
+
+    public void setTicketTillStopSeqNo(int ticketTillStopSeqNo) {
+        this.ticketTillStopSeqNo = ticketTillStopSeqNo;
+    }
+
+    public String getTicketDate() {
+        return ticketDate;
+    }
+
+    public void setTicketDate(String ticketDate) {
+        this.ticketDate = ticketDate;
+    }
+
+    public String getTicketTime() {
+        return ticketTime;
+    }
+
+    public void setTicketTime(String ticketTime) {
+        this.ticketTime = ticketTime;
+    }
+
+    public int getTotalTicketAmount() {
+        return totalTicketAmount;
+    }
+
+    public void setTotalTicketAmount(int totalTicketAmount) {
+        this.totalTicketAmount = totalTicketAmount;
+    }
+
+    public double getTravelledKm() {
+        return travelledKm;
+    }
+
+    public void setTravelledKm(double travelledKm) {
         this.travelledKm = travelledKm;
     }
 }
