3f7c26a5c4219e4ba750fd2261e786a288f09a17
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 12:19:19 +0530

    created functional interface to calculate the total ticket amount

--- src/main/java/com/jap/ticketing/service/TotalTicket.java
+++ src/main/java/com/jap/ticketing/service/TotalTicket.java
@@ -1,5 +1,9 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.ticket.Model;
+
+import java.util.List;
+
 public interface TotalTicket {
-    int totalCollectedTicketAmount(int totalAmount);
+    int totalCollectedTicketAmount(List<Model> modelList);
 }
