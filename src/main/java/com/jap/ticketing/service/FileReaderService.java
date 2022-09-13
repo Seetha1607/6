40ab06bfb8db68bfdf89b4f4b6bc6574ecba0ae4
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 10:43:50 +0530

    implemented the logic to read file line by line and handled the exceptions

--- src/main/java/com/jap/ticketing/service/FileReaderService.java
+++ src/main/java/com/jap/ticketing/service/FileReaderService.java
@@ -9,10 +9,36 @@ package com.jap.ticketing.service;
 
 import com.jap.ticketing.model.ticket.Model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileReaderService {
     public List<Model> readFileLineByLine(String fileName) {
-        return null;
+        List<Model> modelList = new ArrayList<>();
+
+        try (FileReader fileReader = new FileReader("src/main/resources/resources/sample.csv")) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] split = line.split(",");
+                String scheduleNumber = split[0].trim();
+                String routeNumber = split[1].trim();
+                int ticketStopFromId = Integer.parseInt(split[2].trim());
+                int ticketFromStopSequenceNumber = Integer.parseInt(split[3].trim());
+                int ticketTillStopId = Integer.parseInt(split[4].trim());
+                int ticketTillStopSeqNo = Integer.parseInt(split[5].trim());
+                String ticketDate = split[6].trim();
+                String ticketTime = split[7].trim();
+                int totalTicketAmount = Integer.parseInt(split[8].trim());
+                double travelledKm = Double.parseDouble(split[9].trim());
+                modelList.add(new Model(scheduleNumber, routeNumber, ticketStopFromId, ticketFromStopSequenceNumber, ticketTillStopId, ticketTillStopSeqNo, ticketDate, ticketTime, totalTicketAmount, travelledKm));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return modelList;
     }
 }
