cc21c390260f6457f19adccb9e2164b09f4afe7e
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 14:20:14 +0530

    created test cases for DataService class in DataServiceTest

--- src/test/java/com/jap/ticketing/service/DataServiceTest.java
+++ src/test/java/com/jap/ticketing/service/DataServiceTest.java
@@ -1,24 +1,41 @@
 package com.jap.ticketing.service;
 
+import com.jap.ticketing.model.ticket.Model;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DataServiceTest {
+    DataService dataService = null;
+    FileReaderService fileReaderService = null;
+    String fileName = "src/test/resources/sample.csv";
 
     @BeforeEach
     void setUp() {
+        dataService = new DataService();
+        fileReaderService = new FileReaderService();
     }
 
     @AfterEach
     void tearDown() {
+        dataService = null;
+        fileReaderService = null;
     }
 
     @Test
-    void getSortedDistanceTravelled() {
+    public void getSortedDistanceTravelledSuccess() {
+        List<Model> expectedOutput = fileReaderService.readFileLineByLine(fileName);
+        assertEquals(49.5, dataService.getSortedDistanceTravelled(expectedOutput).get(0).getTravelledKm(), 0);
     }
 
     @Test
-    void getTotalAmountCollected() {
+    public void getTotalAmountCollectedSuccess() {
+        List<Model> expectedOutput = fileReaderService.readFileLineByLine(fileName);
+        assertEquals(10348, dataService.getTotalAmountCollected(expectedOutput));
     }
+
 }
