ff84ecf1152b3ff7efabca3d529fceee13a1a6c5
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 11:05:21 +0530

    written the logic to sort the distance by descending order in the method

--- src/main/java/com/jap/ticketing/service/DataService.java
+++ src/main/java/com/jap/ticketing/service/DataService.java
@@ -9,10 +9,15 @@ package com.jap.ticketing.service;
 
 import com.jap.ticketing.model.ticket.Model;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class DataService {
     public List<Model> getSortedDistanceTravelled(List<Model> serviceData) {
-        return null;
+        Comparator<Model> distanceComparator = (((o1, o2) -> Double.compare(o2.getTravelledKm(), o1.getTravelledKm())));
+        serviceData.sort(distanceComparator);
+        for (Model distance : serviceData)
+            System.out.println(distance.getTravelledKm());
+        return serviceData;
     }
 }
