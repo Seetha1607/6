c109a79d0f4a27b002cb2b8c958f6b8ce465fe5e
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 12:54:48 +0530

    created object of FileReaderService class name and called the methods to print the output

--- src/main/java/com/jap/ticketing/Hello.java
+++ src/main/java/com/jap/ticketing/Hello.java
@@ -1,8 +1,14 @@
 package com.jap.ticketing;
 
-public class Hello {
+import com.jap.ticketing.model.ticket.Model;
+import com.jap.ticketing.service.FileReaderService;
 
+import java.util.List;
+
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        FileReaderService fileReaderService = new FileReaderService();
+        List<Model> modelList = fileReaderService.readFileLineByLine("src/main/resources/resources/sample.csv");
+        System.out.println(modelList);
     }
 }
