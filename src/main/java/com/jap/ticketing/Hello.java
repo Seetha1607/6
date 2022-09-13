310f1128743ea1b81d800c8fa00e742f253bc31f
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 12:59:06 +0530

    created object of DataService class name and called the method to sort the distance by descending order

--- src/main/java/com/jap/ticketing/Hello.java
+++ src/main/java/com/jap/ticketing/Hello.java
@@ -1,14 +1,25 @@
 package com.jap.ticketing;
 
 import com.jap.ticketing.model.ticket.Model;
+import com.jap.ticketing.service.DataService;
 import com.jap.ticketing.service.FileReaderService;
 
 import java.util.List;
 
 public class Hello {
     public static void main(String[] args) {
+        // Creating an instance of the FileReaderService class.
         FileReaderService fileReaderService = new FileReaderService();
+        // Reading the file line by line and storing it in a list.
         List<Model> modelList = fileReaderService.readFileLineByLine("src/main/resources/resources/sample.csv");
+        // Printing the list of models.
         System.out.println(modelList);
+
+        // Creating an instance of the DataService class.
+        DataService dataService = new DataService();
+        // Calling the getSortedDistanceTravelled method in the DataService class and passing the modelList as a parameter.
+        List<Model> sortedDistanceTravelled = dataService.getSortedDistanceTravelled(modelList);
+        // Printing the sortedDistanceTravelled list.
+        System.out.println(sortedDistanceTravelled);
     }
 }
