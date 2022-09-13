63ca174868de5a2e256828711cd1b1c4105d9913
Seetha Lakshmi.G.M <seethalakshmi1607@gmail.com>
2022-09-13 12:36:17 +0530

    created FileReaderServiceTest and DataServiceTest files

---
+++ src/test/java/com/jap/ticketing/service/DataServiceTest.java
@@ -0,0 +1,24 @@
+package com.jap.ticketing.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DataServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSortedDistanceTravelled() {
+    }
+
+    @Test
+    void getTotalAmountCollected() {
+    }
+}
---
+++ src/test/java/com/jap/ticketing/service/FileReaderServiceTest.java
@@ -0,0 +1,20 @@
+package com.jap.ticketing.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FileReaderServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void readFileLineByLine() {
+    }
+}
